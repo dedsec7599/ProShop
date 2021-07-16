9767b270610539ac8fd8a1e1830f9189fc71485d
dedsec7599 <theingeniousone@9912>
2021-07-16 17:08:57 +0530

    Admin User - Check users list and security issues

--- frontend/src/screens/PaymentScreen.js
+++ frontend/src/screens/PaymentScreen.js
@@ -26,7 +26,7 @@ const PaymentScreen = ({ history }) => {
   return (
     <FormContainer>
       <CheckoutSteps step1 step2 step3 />
-      <h1>Payemnt Method</h1>
+      <h1>Payment Method</h1>
       <Form onSubmit={submitHandler}>
         <Form.Group>
           <Form.Label as="legend">Select Method</Form.Label>
